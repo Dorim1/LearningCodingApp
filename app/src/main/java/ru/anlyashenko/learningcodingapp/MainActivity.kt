178fad7150f6b89ec77161eaa1d6a2a41f21f03d
Dreamy <tolya.lyashhenko.05@mail.ru>
2025-08-03 16:21:52 +0300

    LCA-L-7-work with MainActivity.kt

--- app/src/main/java/ru/anlyashenko/learningcodingapp/MainActivity.kt
+++ app/src/main/java/ru/anlyashenko/learningcodingapp/MainActivity.kt
@@ -1,21 +1,47 @@
 package ru.anlyashenko.learningcodingapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import ru.anlyashenko.learningcodingapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityLearnWordBinding
+
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-        setContentView(R.layout.activity_learn_word)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // findViewById
+        // ViewBinding
+
+//        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
+//        tvQuestionWord.text = "42"
+//        tvQuestionWord.setTextColor(Color.BLUE)
+//        tvQuestionWord.setTextColor(Color.parseColor("#FDD600"))
+//        tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.buttonBlueColor))
+
+        with(binding) {
+            tvQuestionWord.text = "AndroidSprint.ru"
+            tvQuestionWord.setTextColor(Color.BLUE)
+//            tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.buttonBlueColor))
+            imageButton.isVisible = false
+        }
+
+
 
     }
 }
