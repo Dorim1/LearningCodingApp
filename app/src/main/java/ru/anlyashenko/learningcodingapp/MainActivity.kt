b844b292000883253375b00b2a811015004fb74b
Dreamy <tolya.lyashhenko.05@mail.ru>
2025-08-04 20:33:19 +0300

    LCA-L-8-the answer button click has been processed

--- app/src/main/java/ru/anlyashenko/learningcodingapp/MainActivity.kt
+++ app/src/main/java/ru/anlyashenko/learningcodingapp/MainActivity.kt
@@ -13,8 +13,6 @@ import ru.anlyashenko.learningcodingapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityLearnWordBinding
-
     private var _binding: ActivityLearnWordBinding? = null
     private val binding
         get() = _binding ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
@@ -24,24 +22,29 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityLearnWordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        // findViewById
-        // ViewBinding
-
-//        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
-//        tvQuestionWord.text = "42"
-//        tvQuestionWord.setTextColor(Color.BLUE)
-//        tvQuestionWord.setTextColor(Color.parseColor("#FDD600"))
-//        tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.buttonBlueColor))
-
-        with(binding) {
-            tvQuestionWord.text = "AndroidSprint.ru"
-            tvQuestionWord.setTextColor(Color.BLUE)
-//            tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.buttonBlueColor))
-            imageButton.isVisible = false
+        binding.layoutAnswer3.setOnClickListener {
+            markAnswerCorrect()
         }
 
 
+    }
 
+    private fun markAnswerCorrect() {
+        binding.layoutAnswer3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_correct
+        )
+
+        binding.tvVariantNumber3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_correct
+        )
+
+        binding.tvVariantNumber3.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.white))
+
+        binding.tvVariantValue3.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctVariantsColor))
     }
 }
