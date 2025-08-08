ad78440c2eded5ac8479cf91843612fc3994ff96
Dreamy <tolya.lyashhenko.05@mail.ru>
2025-08-08 17:40:19 +0300

    LCA-L-10-12-theory

---
+++ app/src/main/java/ru/anlyashenko/learningcodingapp/BatteryLevelReceiver.kt
@@ -0,0 +1,14 @@
+package ru.anlyashenko.learningcodingapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BatteryLevelReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+    // Способы регистрации:
+    // Статический, динамический
+}
---
+++ app/src/main/java/ru/anlyashenko/learningcodingapp/MusicService.kt
@@ -0,0 +1,28 @@
+package ru.anlyashenko.learningcodingapp
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+
+class MusicService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStart(intent: Intent?, startId: Int) {
+        super.onStart(intent, startId)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    // Unbound: Foreground Service, Background Service
+    // Bound Service
+
+}
